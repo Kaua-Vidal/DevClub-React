53683fe0c3e0620f9f23805986654c5df119e5cd
Kaua-Vidal <kauakauavidalpc@gmail.com>
2025-10-14 00:45:28 -0300

    styling using css

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import { Title, Container, TopBackground, Form, ContainerInputs, Input, Button, InputLabel } from './styles'
+import UsersImage from './assets/users.png'
 
 function Home() {
 
   return (
     <Container>
       <TopBackground>
-        <img src="" alt="" />
+        <img src={UsersImage} alt="imagem-usuarios" />
       </TopBackground>
 
       <Form form action="">
-        <Title>ok react</Title>
+        <Title>Cadastrar Usuário</Title>
 
         <ContainerInputs>
-          <div>
             <div>
               <InputLabel>
-                Nome<span>*</span>
+                Nome<span> *</span>
               </InputLabel>
               <Input type='text' placeholder='Nome do usuário' />
             </div>
 
             <div>
               <InputLabel>
-                Idade<span>*</span>
+                Idade<span> *</span>
               </InputLabel>
               <Input type='number' placeholder='Idade do usuário' />
             </div>
-          </div>
 
-          <div>
+          
+        </ContainerInputs>
+
+        <div style={{width: '100%'}}>
             <InputLabel>
-              E-mail<span>*</span>
+              E-mail<span> *</span>
             </InputLabel>
             <Input type='email' placeholder='E-mail do usuário' />
           </div>
-        </ContainerInputs>
-
         <Button>Cadastrar Usuário</Button>
       </Form>
     </Container>
