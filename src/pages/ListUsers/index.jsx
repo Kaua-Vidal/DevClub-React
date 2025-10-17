44893b115ad6bcfb1a97080b905662a8047dd7c8
Kaua-Vidal <kauakauavidalpc@gmail.com>
2025-10-16 23:43:15 -0300

    styling the second page and adding the delete method

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,13 +1,17 @@
+import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import api from '../../services/api'
 import Button from '../../components/Button'
 import TopBackground from '../../components/TopBackground'
 import UsersImage from '../../assets/users.png'
 import { func } from 'prop-types'
+import Trash from '../../assets/trash.svg'
+import { CardUsers, Container, ContainerUsers, TrashIcon, Title, AvatarUser } from './styles'
 
 function ListUsers() {
 
-const [users, setUsers] = useState([])
+    const navigate = useNavigate()
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
         async function getUsers() {
@@ -21,24 +25,38 @@ const [users, setUsers] = useState([])
     //TODA VEZ que a tela carrega, o useEffect é chamado
     //TODA VEZ que uma determinada variavel MUDA de valor, ele é chamado
 
+    async function deleteUsers(id) {
+        await api.delete(`usuarios/${id}`)
+
+    const updatedUsers = users.filter(user => user.id !== id)
+    
+    setUsers(updatedUsers)
+    }
+
 
     return (
+        <Container>
+            <TopBackground />
+            <Title>Lista de Usuários</Title>
 
+            <ContainerUsers>
 
-        <div>
-            <TopBackground/>
-            <h1>Listagem de Usuários</h1>
+                {users.map(user => (
+                    <CardUsers key={user.id}>
+                        <AvatarUser src={`https://avatar.iran.liara.run/public?username=${user.id}`}/>
+                        <div >
+                            <h3>{user.name}</h3>
+                            <p>{user.age}</p>
+                            <p>{user.email}</p>
+                        </div>
+                        <TrashIcon src={Trash} alt='Lixo-img' onClick={() => deleteUsers(user.id)}/>
+                    </CardUsers>
+                ))}
+            </ContainerUsers>
 
-            {users.map( user => (
-                <div key={user.id}>
-                    <p>{user.name}</p>
-                    <p>{user.age}</p>
-                    <p>{user.email}</p>
-                </div>
-            ))}
-            <Button>Voltar</Button>
+            <Button type="button" onClick={() => navigate('/')}>Voltar</Button>
 
-        </div>
+        </Container>
     )
 }
 
